62fdceecb6b381fda838009dd17f1489ad714fe9
traqel <traqel@gmail.com>
2022-11-21 18:17:23 +0400

    Test update

--- test/Pool.ts
+++ test/Pool.ts
@@ -7,8 +7,5 @@ describe("Pool contract", function () {
     it("Some func", async function () {
         const {poolFactory} = await loadFixture(deployTokenFixture);
         await poolFactory.pause();
-        expect(await poolFactory.paused()).to.equal(true);
-        await poolFactory.unpause();
-        expect(await poolFactory.paused()).to.equal(false);
     });
 });

6d4eb8c8498b547002bbf1fd9e5eb2d86646c676
Tigran Arakelyan <tigranarakelyan@Tigrans-Mac-mini.local>
2023-11-23 10:56:59 +0400

    Add forge

---
+++ Makefile
@@ -0,0 +1,50 @@
+# include .env file and export its env vars
+# (-include to ignore error if it does not exist)
+-include .env
+
+all: clean remove install update build
+
+# Clean the repo
+clean-all:
+	forge clean
+
+# Remove modules
+remove:
+	rm -rf .gitmodules
+	rm -rf .git/modules/*
+	rm -rf lib
+	touch .gitmodules
+	git add .
+	git commit -m "modules"
+
+# Install the Modules
+install:
+	forge install foundry-rs/forge-std
+	forge install OpenZeppelin/openzeppelin-contracts
+
+# Update Dependencies
+update:
+	forge update
+
+remap:
+	forge remappings > remappings.txt
+
+# Builds
+build:
+	forge clean
+	forge remappings > remappings.txt
+	forge build
+
+deploy:
+	forge create StakeContract --private-key ${PRIVATE_KEY} # --rpc-url
+
+format:
+	forge fmt
+
+test-all:
+	forge test -vvv --gas-report
+	forge coverage
+
+
+
+
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,102 @@
-import { HardhatUserConfig } from "hardhat/config";
+// noinspection JSUnusedGlobalSymbols
+import "dotenv/config";
+import {HardhatUserConfig} from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-abi-exporter";
+import "hardhat-storage-layout";
+import "hardhat-contract-sizer";
+import "@nomiclabs/hardhat-etherscan";
+import "hardhat-deploy";
+import "@nomicfoundation/hardhat-foundry";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.16",
-};
+    solidity: {
+        version: "0.8.16",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 300,
+            },
+            outputSelection: {
+                "*": {
+                    "*": ["storageLayout"],
+                },
+            }
+        },
+    },
+    namedAccounts: {
+        owner: 0,
+        admin: 1,
+        user: 2,
+        borrower: 3
+    },
+    gasReporter: {
+        enabled: true,
+        currency: "USD",
+        url: "http://localhost:8545",
+        coinmarketcap: process.env.COIN_MARKET_CAP
+    },
+    abiExporter: {
+        path: './abi',
+        only: [
+            'Pool',
+            'PoolFactory',
+            'HeliosGlobals',
+            'LiquidityLocker',
+            'LiquidityLockerFactory',
+            'IERC20Metadata',
+            'IERC20',
+        ],
+        runOnCompile: true,
+        flat: true,
+        spacing: 2,
+        pretty: false,
+    },
 
-export default config;
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            forking: {
+                enabled: true,
+                url: process.env.MAIN_NET_FORK_API_KEY!,
+                blockNumber: 17090000
+            }
+        },
+        alfajores: {
+            url: process.env.CELO_API_KEY,
+            accounts: [
+                process.env.CELO_OWNER_PRIVATE_KEY!,
+                process.env.CELO_ADMIN_PRIVATE_KEY!,
+                process.env.CELO_USER_PRIVATE_KEY!,
+                process.env.CELO_BORROWER_PRIVATE_KEY!,
+            ],
+            chainId: 44787,
+        },
+        goerli: {
+            url: process.env.GOERLI_API_KEY,
+            accounts: [
+                process.env.GOERLI_OWNER_PRIVATE_KEY!,
+                process.env.GOERLI_ADMIN_PRIVATE_KEY!,
+                process.env.GOERLI_USER_PRIVATE_KEY!,
+                process.env.GOERLI_BORROWER_PRIVATE_KEY!
+            ]
+        },
+    },
+    etherscan: {
+        apiKey: {
+            goerli: process.env.ETHERSCAN_API_KEY!
+        },
+        customChains: [
+            {
+                network: "goerli",
+                chainId: 5,
+                urls: {
+                    apiURL: "https://api-goerli.etherscan.io/api",
+                    browserURL: "https://goerli.etherscan.io"
+                }
+            }
+        ]
+    }
+}
+
+    export default config;

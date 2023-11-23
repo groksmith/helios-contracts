e9e4718e0829f48132f95df2f0478ae9cc9dc344
Tigran Arakelyan <tigranarakelyan@Tigrans-Mac-mini.local>
2023-11-23 16:08:11 +0400

    Code cleanup

--- forge-test/FixtureContract.sol
+++ forge-test/FixtureContract.sol
@@ -1,10 +1,24 @@
 pragma solidity 0.8.16;
 
 import "forge-std/Test.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {HeliosGlobals} from "../contracts/global/HeliosGlobals.sol";
+import {LiquidityLockerFactory} from "../contracts/pool/LiquidityLockerFactory.sol";
+import {PoolFactory} from "../contracts/pool/PoolFactory.sol";
 
 abstract contract FixtureContract {
     address public constant OWNER_ADDRESS = 0x8A867fcC5a4d1FBbf7c1A9D6e5306b78511fDDDe;
     address public constant ADMIN_ADDRESS = 0x4F8fF72C3A17B571D4a1671d5ddFbcf48187FBCa;
-    address public constant POOL_FACTORY_ADDRESS = 0x0E4A822e7Dd9dB6CCa24cBA5814961BcFA3ae64e;
-    address public constant LIQUIDITY_ASSET_ADDRESS = 0x07865c6E87B9F70255377e024ace6630C1Eaa37F;
+
+    HeliosGlobals public heliosGlobals;
+    ERC20 public liquidityAsset;
+    PoolFactory public poolFactory;
+    LiquidityLockerFactory public liquidityLockerFactory;
+
+    function fixture() public{
+        heliosGlobals = new HeliosGlobals(OWNER_ADDRESS, ADMIN_ADDRESS);
+        liquidityAsset = new ERC20("USDT", "USDT");
+        poolFactory = new PoolFactory(address(heliosGlobals));
+        liquidityLockerFactory = new LiquidityLockerFactory();
+    }
 }
--- forge-test/HeliosGlobalsTest.sol
+++ forge-test/HeliosGlobalsTest.sol
@@ -1,14 +1,12 @@
 pragma solidity 0.8.16;
 
 import "forge-std/Test.sol";
-import "../contracts/global/HeliosGlobals.sol";
 import {FixtureContract} from "./FixtureContract.sol";
 
 contract HeliosGlobalsTest is Test, FixtureContract {
-    HeliosGlobals public heliosGlobals;
 
     function setUp() public {
-        heliosGlobals = new HeliosGlobals(OWNER_ADDRESS, ADMIN_ADDRESS);
+        fixture();
     }
 
     function test_adminSetPaused() public {
@@ -30,7 +28,7 @@ contract HeliosGlobalsTest is Test, FixtureContract {
         vm.stopPrank();
     }
 
-    function test_not_owner_setPaused(address user) public {
+    function test_when_not_owner_setPaused(address user) public {
         vm.assume(user != OWNER_ADDRESS);
         vm.startPrank(user);
 
@@ -47,14 +45,12 @@ contract HeliosGlobalsTest is Test, FixtureContract {
         vm.stopPrank();
     }
 
-    function test_when_owner_setValidPoolFactory(address poolFactory) public {
-        vm.assume(poolFactory != POOL_FACTORY_ADDRESS);
+    function test_when_owner_setValidPoolFactory() public {
         vm.startPrank(OWNER_ADDRESS);
 
-        heliosGlobals.setValidPoolFactory(POOL_FACTORY_ADDRESS, true);
-
-        assertEq(heliosGlobals.isValidPoolFactory(POOL_FACTORY_ADDRESS), true);
-        assertEq(heliosGlobals.isValidPoolFactory(poolFactory), false);
+        address poolFactoryAddress = address(poolFactory);
+        heliosGlobals.setValidPoolFactory(poolFactoryAddress, true);
+        assertEq(heliosGlobals.isValidPoolFactory(poolFactoryAddress), true);
 
         vm.stopPrank();
     }
@@ -63,91 +59,133 @@ contract HeliosGlobalsTest is Test, FixtureContract {
         vm.assume(user != OWNER_ADDRESS);
         vm.startPrank(user);
 
+        address poolFactoryAddress = address(poolFactory);
         vm.expectRevert(bytes("MG:NOT_GOV"));
-        heliosGlobals.setValidPoolFactory(POOL_FACTORY_ADDRESS, true);
-
-        assertEq(heliosGlobals.isValidPoolFactory(POOL_FACTORY_ADDRESS), false);
+        heliosGlobals.setValidPoolFactory(poolFactoryAddress, true);
+        assertEq(heliosGlobals.isValidPoolFactory(poolFactoryAddress), false);
 
         vm.stopPrank();
     }
-//
-//    function test_when_owner_setPoolDelegateAllowList(address poolDelegate) public {
-//        vm.startPrank(OWNER_ADDRESS);
-//
-//        assertEq(heliosGlobals.isValidPoolDelegate(poolDelegate), false);
-//        heliosGlobals.setPoolDelegateAllowList(poolDelegate, true);
-//        assertEq(heliosGlobals.isValidPoolDelegate(poolDelegate), true);
-//
-//        vm.stopPrank();
-//    }
-//
-//    function test_when_not_owner_setPoolDelegateAllowList(address user, address poolDelegate) public {
-//        vm.startPrank(user);
-//
-//        vm.expectRevert(bytes("MG:NOT_GOV"));
-//        heliosGlobals.setPoolDelegateAllowList(poolDelegate, true);
-//
-//        assertEq(heliosGlobals.isValidPoolDelegate(poolDelegate), false);
-//
-//        vm.stopPrank();
-//    }
-//
-//    function test_when_owner_setGlobalAdmin(address newAdmin) public {
-//        assertNotEq(heliosGlobals.globalAdmin(), newAdmin);
-//
-//        vm.startPrank(OWNER_ADDRESS);
-//        heliosGlobals.setGlobalAdmin(newAdmin);
-//        vm.stopPrank();
-//
-//        assertEq(heliosGlobals.globalAdmin(), newAdmin);
-//    }
-//
-//    function test_when_paused_setGlobalAdmin(address newAdmin) public {
-//
-//        vm.startPrank(ADMIN_ADDRESS);
-//        heliosGlobals.setProtocolPause(true);
-//        vm.stopPrank();
-//
-//        vm.startPrank(OWNER_ADDRESS);
-//        vm.expectRevert(bytes("HG:PROTO_PAUSED"));
-//        heliosGlobals.setGlobalAdmin(newAdmin);
-//        vm.stopPrank();
-//
-//        vm.startPrank(ADMIN_ADDRESS);
-//        heliosGlobals.setProtocolPause(false);
-//        vm.stopPrank();
-//    }
-//
-//    function test_when_not_owner_setGlobalAdmin(address user, address globalAdmin) public {
-//        assertNotEq(heliosGlobals.globalAdmin(), globalAdmin);
-//
-//        vm.startPrank(user);
-//        vm.expectRevert(bytes("HG:NOT_GOV_OR_ADM"));
-//        heliosGlobals.setGlobalAdmin(globalAdmin);
-//        vm.stopPrank();
-//
-//        assertNotEq(heliosGlobals.globalAdmin(), globalAdmin);
-//    }
-//
-//    function test_when_owner_setLiquidityAsset(address liquidityAsset) public {
-//        vm.startPrank(OWNER_ADDRESS);
-//
-//        heliosGlobals.setLiquidityAsset(LIQUIDITY_ASSET_ADDRESS, true);
-//
-//        assertEq(heliosGlobals.isValidLiquidityAsset(LIQUIDITY_ASSET_ADDRESS), true);
-//        assertEq(heliosGlobals.isValidLiquidityAsset(liquidityAsset), false);
-//
-//        vm.stopPrank();
-//    }
 
-//    function test_when_not_owner_setLiquidityAsset() public {
-//        vm.startPrank(address(1));
-//
-//        vm.expectRevert(bytes("MG:NOT_GOV"));
-//        heliosGlobals.setLiquidityAsset(LIQUIDITY_ASSET_ADDRESS, true);
-//
-//        assertEq(heliosGlobals.isValidLiquidityAsset(LIQUIDITY_ASSET_ADDRESS), false);
-//
-//        vm.stopPrank();
-//    }
+    function test_when_owner_setPoolDelegateAllowList(address poolDelegate) public {
+        vm.startPrank(OWNER_ADDRESS);
+
+        assertEq(heliosGlobals.isValidPoolDelegate(poolDelegate), false);
+        heliosGlobals.setPoolDelegateAllowList(poolDelegate, true);
+        assertEq(heliosGlobals.isValidPoolDelegate(poolDelegate), true);
+
+        vm.stopPrank();
+    }
+
+    function test_when_not_owner_setPoolDelegateAllowList(address user, address poolDelegate) public {
+        vm.assume(user != OWNER_ADDRESS);
+        vm.startPrank(user);
+
+        vm.expectRevert(bytes("MG:NOT_GOV"));
+        heliosGlobals.setPoolDelegateAllowList(poolDelegate, true);
+        assertEq(heliosGlobals.isValidPoolDelegate(poolDelegate), false);
+
+        vm.stopPrank();
+    }
+
+    function test_when_owner_setGlobalAdmin(address newAdmin) public {
+        vm.assume(newAdmin != address(0) && newAdmin != heliosGlobals.globalAdmin());
+        assertNotEq(heliosGlobals.globalAdmin(), newAdmin);
+
+        vm.startPrank(OWNER_ADDRESS);
+        heliosGlobals.setGlobalAdmin(newAdmin);
+        vm.stopPrank();
+        assertEq(heliosGlobals.globalAdmin(), newAdmin);
+    }
+
+    function test_when_owner_set_zero_setGlobalAdmin() public {
+        address newAdmin = address(0);
+
+        vm.startPrank(OWNER_ADDRESS);
+        vm.expectRevert("HG:NOT_GOV_OR_ADM");
+        heliosGlobals.setGlobalAdmin(newAdmin);
+
+        assertNotEq(heliosGlobals.globalAdmin(), newAdmin);
+        vm.stopPrank();
+    }
+
+    function test_when_paused_setGlobalAdmin(address newAdmin) public {
+        vm.startPrank(ADMIN_ADDRESS);
+        heliosGlobals.setProtocolPause(true);
+        vm.stopPrank();
+
+        vm.startPrank(OWNER_ADDRESS);
+        vm.expectRevert(bytes("HG:PROTO_PAUSED"));
+        heliosGlobals.setGlobalAdmin(newAdmin);
+        vm.stopPrank();
+
+        vm.startPrank(ADMIN_ADDRESS);
+        heliosGlobals.setProtocolPause(false);
+        vm.stopPrank();
+    }
+
+    function test_when_not_owner_setGlobalAdmin(address user, address globalAdmin) public {
+        vm.assume(user != OWNER_ADDRESS);
+        assertNotEq(heliosGlobals.globalAdmin(), globalAdmin);
+
+        vm.startPrank(user);
+        vm.expectRevert(bytes("HG:NOT_GOV_OR_ADM"));
+        heliosGlobals.setGlobalAdmin(globalAdmin);
+        vm.stopPrank();
+
+        assertNotEq(heliosGlobals.globalAdmin(), globalAdmin);
+    }
+
+    function test_when_owner_setLiquidityAsset() public {
+        vm.startPrank(OWNER_ADDRESS);
+
+        address liquidityAssetAddress = address(liquidityAsset);
+        heliosGlobals.setLiquidityAsset(liquidityAssetAddress, true);
+        assertEq(heliosGlobals.isValidLiquidityAsset(liquidityAssetAddress), true);
+
+        vm.stopPrank();
+    }
+
+    function test_when_not_owner_setLiquidityAsset(address user) public {
+        vm.assume(user != OWNER_ADDRESS);
+        vm.startPrank(user);
+
+        address liquidityAssetAddress = address(liquidityAsset);
+        vm.expectRevert(bytes("MG:NOT_GOV"));
+        heliosGlobals.setLiquidityAsset(liquidityAssetAddress, true);
+        assertEq(heliosGlobals.isValidLiquidityAsset(liquidityAssetAddress), false);
+
+        vm.stopPrank();
+    }
+
+    function test_when_owner_setValidSubFactory() public {
+        vm.startPrank(OWNER_ADDRESS);
+
+        address poolFactoryAddress = address(poolFactory);
+        address liquidityLockerFactoryAddress = address(liquidityLockerFactory);
+
+        heliosGlobals.setValidPoolFactory(poolFactoryAddress, true);
+        heliosGlobals.setValidSubFactory(poolFactoryAddress, liquidityLockerFactoryAddress, true);
+        assertEq(heliosGlobals.isValidSubFactory(poolFactoryAddress, liquidityLockerFactoryAddress, 1), true);
+        assertEq(heliosGlobals.isValidSubFactory(poolFactoryAddress, liquidityLockerFactoryAddress, 2), false);
+
+        vm.stopPrank();
+    }
+
+    function test_when_not_owner_setValidSubFactory(address user) public {
+        vm.assume(user != OWNER_ADDRESS);
+        vm.startPrank(OWNER_ADDRESS);
+
+        address poolFactoryAddress = address(poolFactory);
+        address liquidityLockerFactoryAddress = address(liquidityLockerFactory);
+
+        heliosGlobals.setValidPoolFactory(poolFactoryAddress, true);
+        vm.stopPrank();
+
+        vm.startPrank(user);
+        vm.expectRevert(bytes("MG:NOT_GOV"));
+        heliosGlobals.setValidSubFactory(poolFactoryAddress, liquidityLockerFactoryAddress, true);
+        vm.stopPrank();
+    }
+
 }

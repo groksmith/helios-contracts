2f226c34fe08df3e6b24e057548e2639e0322b8d
Tigran Arakelyan <tigranarakelyan@Tigrans-Mac-mini.local>
2024-01-25 16:46:21 +0400

    Globals tests cleanup

--- contracts/global/HeliosGlobals.sol
+++ contracts/global/HeliosGlobals.sol
@@ -13,13 +13,13 @@ contract HeliosGlobals is AccessControl, IHeliosGlobals {
     bool public override protocolPaused; // Switch to pause the functionality of the entire protocol.
     mapping(address => bool) public override isValidPoolFactory; // Mapping of valid Pool Factories
     mapping(address => bool) public override isValidLiquidityAsset; // Mapping of valid Liquidity Assets
-    mapping(address => mapping(address => bool)) public override validSubFactories;
+    mapping(address => bool) public override isValidLiquidityLockerFactory;
 
     event ProtocolPaused(bool pause);
     event Initialized();
     event LiquidityAssetSet(address asset, uint256 decimals, string symbol, bool valid);
     event ValidPoolFactorySet(address indexed poolFactory, bool valid);
-    event ValidSubFactorySet(address indexed superFactory, address indexed subFactory, bool valid);
+    event ValidLiquidityLockerFactorySet(address indexed liquidityLockerFactory, bool valid);
 
     constructor(address admin) {
         _setupRole(DEFAULT_ADMIN_ROLE, admin);
@@ -44,25 +44,15 @@ contract HeliosGlobals is AccessControl, IHeliosGlobals {
     }
 
     // Sets the validity of a sub factory as it relates to a super factory. Only the Admin can call this function
-    function setValidSubFactory(address superFactory, address subFactory, bool valid) external onlyAdmin {
-        require(isValidPoolFactory[superFactory], "HG:INV_SUPER_F");
-        validSubFactories[superFactory][subFactory] = valid;
-        emit ValidSubFactorySet(superFactory, subFactory, valid);
+    function setValidLiquidityLockerFactory(address liquidityLockerFactory, bool valid) external onlyAdmin {
+        isValidLiquidityLockerFactory[liquidityLockerFactory] = valid;
+        emit ValidLiquidityLockerFactorySet(liquidityLockerFactory, valid);
     }
 
     // Sets the validity of an asset for liquidity in Pools. Only the Admin can call this function
     function setLiquidityAsset(address asset, bool valid) external onlyAdmin {
         isValidLiquidityAsset[asset] = valid;
         emit LiquidityAssetSet(asset, IERC20Metadata(asset).decimals(), IERC20Metadata(asset).symbol(), valid);
-    }
-
-    // Checks that a subFactory is valid as it relates to a super factory
-    function isValidSubFactory(address superFactory, address subFactory, uint8 factoryType)
-        external
-        view
-        returns (bool)
-    {
-        return validSubFactories[superFactory][subFactory] && ISubFactory(subFactory).factoryType() == factoryType;
     }
 
     /// @dev Restricted to members of the admin role.
